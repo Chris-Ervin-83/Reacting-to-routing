3e794dfa25ff0c87405e97ac9440d4fb2beff756
Christopher Ervin <manofzion@icloud.com>
2022-03-22 13:07:32 -0500

    still working on it

---
+++ src/pages/films.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import FilmsCard from '../components/FilmsCard';
+
+const Films = () => {
+    const [films, setFilms] = useState([])
+
+    useEffect(() => {
+        const getFilms = async () => {
+            const res = await fetch("https://ghibliapi.herokuapp.com/films");
+            const allFilms = await res.json();
+            setFilms(allFilms);
+        }
+        getFilms();
+    }, [])
+
+    return (
+        <div className="container mt-3">
+            <div className="row justify-content-center">
+                <h1 className="display-4">Studio Ghibli Films</h1>
+            </div>
+            <div className="row justify-content-center mt-4">
+                {films.map(film => (
+                    <FilmsCard key={film.id} {...film} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default films;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="jumbotron text-center bg-light">
+            <h1 className="display-4">Reacting to Studio Ghibli API</h1>
+            <p className="lead">The Studio Ghibli API catalogs the people, places, and things found in the worlds of Ghibli. It was created to help users discover resources, consume them via HTTP requests, and interact with them in whatever way makes sense. Navigation can be found on the top navbar.</p>
+        </div>
+    );
+};
+
+export default Home;
